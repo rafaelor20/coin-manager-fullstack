4ffa0dc28f50236149001119b878623110ac7d13
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-02-27 23:21:17 -0300

    fix: fix placeholder of input elements

--- front-end/src/components/Form/Input.js
+++ front-end/src/components/Form/Input.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Input = ({ label, type, value, onChange }) => {
   return (
     <div>
-      <label>{label}</label>
-      <input type={type} value={value} onChange={onChange} />
+      <StyledInput placeholder={label} type={type} value={value} onChange={onChange} />
     </div>
   );
 };
 
 export default Input;
+
+const StyledInput = styled.input`
+::placeholder {
+  color: black,
+}`;
