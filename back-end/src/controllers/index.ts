98fdfabd4c47a03176c3cc617b0272ca9c54a062
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-05-28 15:42:47 -0300

    feat: implement tsoa

--- back-end/src/controllers/index.ts
+++ back-end/src/controllers/index.ts
@@ -1,2 +1,5 @@
-export * from './users-controller';
 export * from './authentication-controller';
+export * from './credit-controller';
+export * from './debt-controller';
+export * from './transaction-controller';
+export * from './users-controller';
