d1985a981db4465795500da92acc2b3d1214dc40
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-11-06 13:14:27 -0300

    refactor: move imports to the correct position in DebtPayment component

--- front-end/src/pages/payment/debtPayment.js
+++ front-end/src/pages/payment/debtPayment.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Main, Content, CurrentAmount } from '../../components/styles.js';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
+import { Container, Main, Content, CurrentAmount } from '../../components/styles.js';
 import Page from '../../components/Page.js';
 import Header from '../../components/Header.js';
 import Footer from '../../components/Footer.js';
